081770c817e677d158a87115b0da1e8cd872aec6
Gabbelahdo <156900826+Gabbelahdo@users.noreply.github.com>
2026-01-17 21:35:29 +0100

    Update pokemonSearch.jsx

--- frontend/frontend/src/pokemonSearch.jsx
+++ frontend/frontend/src/pokemonSearch.jsx
@@ -4,15 +4,24 @@ function PokemonSearch(){
     const [search, setSearch] = useState("");
     const [pokemon, setPokemon] = useState(null);
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const API_URL = import.meta.env.VITE_API_URL;
 
 
     const fetchPokemon = async (e) => {
         e.preventDefault();
-        
+        if(!search.trim()){
+            setError("Skriv in ett pokemon-namn");
+            return;
+        }
+
+        setLoading(true);
+        setError("");
+        setPokemon(null);
+
         try{
-            setError("");
+            
 
             const response = await fetch(
                  `${API_URL}/pokemon/${search.toLowerCase()}`
@@ -24,9 +33,11 @@ function PokemonSearch(){
             const data = await response.json();
             setPokemon(data);
         }
- catch{
-    setError("Pokemon existerar ej");
+ catch(err){
+    setError(error.message || "Pokemon existerar ej");
     setPokemon(null);
+ } finally{
+    setLoading(false);
  }
     };
 
@@ -41,14 +52,18 @@ function PokemonSearch(){
 
         <button className="submit-knapp" onClick={fetchPokemon}>Sök</button>
 
+
+        {loading && <p>Laddar Pokemon....</p>}
         {error && <p className="error-msg">{error}</p>}
 
-        {pokemon && (
+        {pokemon && !loading && (
           <>
           <img className="pokemon-bild" src={pokemon.image} alt={pokemon.name} />
           <h2 className="karaktär-namn">Namn: {pokemon.name}</h2>
           <p className="karaktär-typ">Typ: {pokemon.types.join(", ")}</p>
           <p className="karaktär-förmåga">Förmågor: {pokemon.abilities.join(", ")}</p>
+           <p>Vikt: {pokemon.weight} kg</p>
+           <p>Längd: {pokemon.height} m</p>
           </>  
         )
 
